d62cd5ca2bb21a2483e4252918105a485b4fe19f
Omer Enbar <omer@sensibo.com>
2016-09-10 18:01:25 +0300

    Added conversation starters pie chart

--- js/charts.js
+++ js/charts.js
@@ -57,3 +57,38 @@ function emoji_histogram(html_id, name, emojis, limit) {
         }]
     });
 };
+
+function conversation_starters_plot(data) {
+    $("#conversation_starters").highcharts({
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie'
+        },
+        title: {
+            text: 'Who started the conversation?'
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: true,
+                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                    style: {
+                        color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                    }
+                }
+            }
+        },
+        series: [{
+            name: 'names',
+            colorByPoint: true,
+            data: data
+        }]
+    });
+};
